53fc9e8e59ce23458e15eb6fd32ac39d4c905df1
Nucly <nucly@proton.me>
2025-07-01 18:04:53 +0700

    api url

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,2 +1,2 @@
-//export const API = "https://medicant-778013791151.asia-southeast1.run.app/api/";
-export const API = "http://localhost:8080/api/";
+export const API = "https://medicant-778013791151.asia-southeast1.run.app/api/";
+//export const API = "http://localhost:8080/api/";
