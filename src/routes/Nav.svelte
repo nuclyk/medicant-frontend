6d5e48e7d0772f46760b4bdc4426859fd507600a
Nucly <nucly@proton.me>
2025-07-03 20:27:40 +0700

    navbar offset collapse for mobile

--- src/routes/Nav.svelte
+++ src/routes/Nav.svelte
@@ -15,6 +15,7 @@
         >
             <span class="navbar-toggler-icon"></span>
         </button>
+
         <div
             class="offcanvas offcanvas-end"
             tabindex="-1"
@@ -32,16 +33,13 @@
                     aria-label="Close"
                 ></button>
             </div>
+
             <div class="offcanvas-body">
                 <ul class="navbar-nav justify-content-start flex-grow-1 pe-3">
-                    <li class="nav-item">
-                        <a
-                            class="nav-link active"
-                            aria-current="page"
-                            href="/admin">Dashboard</a
-                        >
+                    <li class="nav-item" data-bs-dismiss="offcanvas">
+                        <a class="nav-link" href="/admin">Dashboard</a>
                     </li>
-                    <li class="nav-item">
+                    <li class="nav-item" data-bs-dismiss="offcanvas">
                         <a class="nav-link" href="/admin/manage/users">Manage</a
                         >
                     </li>
@@ -55,12 +53,12 @@
                             For Participants
                         </span>
                         <ul class="dropdown-menu">
-                            <li>
+                            <li data-bs-dismiss="offcanvas">
                                 <a class="dropdown-item" href="/form/checkin"
                                     >Check-in Form</a
                                 >
                             </li>
-                            <li>
+                            <li data-bs-dismiss="offcanvas">
                                 <a class="dropdown-item" href="/form/checkout"
                                     >Check-out Form</a
                                 >
@@ -68,7 +66,7 @@
                             <li>
                                 <hr class="dropdown-divider" />
                             </li>
-                            <li>
+                            <li data-bs-dismiss="offcanvas">
                                 <a
                                     class="dropdown-item"
                                     href="https://drive.google.com/file/d/1bkHNBi1SC7h8odB1kxZ4-qnAI83dS0yr/view?usp=drive_link"
@@ -87,14 +85,14 @@
                             For Volunteers
                         </span>
                         <ul class="dropdown-menu">
-                            <li>
+                            <li data-bs-dismiss="offcanvas">
                                 <a
                                     class="dropdown-item"
                                     href="https://docs.google.com/document/d/13pHelZQkIEyO_3_5vBzJu5vIoPE3h59oeIuxuP4eQy4/edit?tab=t.0"
                                     target="_blank">Retreat Schedules</a
                                 >
                             </li>
-                            <li>
+                            <li data-bs-dismiss="offcanvas">
                                 <a
                                     class="dropdown-item"
                                     href="https://docs.google.com/document/d/1XNbgcbi5iV4hK12yv3WDzxuYoNziLMLDaLavF_hWrSc/edit?tab=t.0"
@@ -112,7 +110,7 @@
                         </ul>
                     </li>
                 </ul>
-                <div class="justify-self-end">
+                <div class="justify-self-end" data-bs-dismiss="offcanvas">
                     {#if !user}
                         <a class="btn text-bg-dark rounded" href="/login"
                             >Login</a
@@ -121,6 +119,7 @@
                         <form action="/login?/logout" method="POST">
                             <button
                                 type="submit"
+                                data-bs-dismiss="offcanvas"
                                 class="btn text-bg-secondary rounded"
                                 >Logout</button
                             >
