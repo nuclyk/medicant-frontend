9cb7f07c9f815d3f51f4ddec713a7d9301543a87
Nucly <nucly@proton.me>
2025-07-03 20:45:19 +0700

    remove .netlify

--- .netlify/server/nodes/3.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 3;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/admin/manage/_layout.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/3.iZrzUKUa.js","_app/immutable/chunks/CWj6FrbW.js","_app/immutable/chunks/DQqt1VYi.js","_app/immutable/chunks/DIeogL5L.js","_app/immutable/chunks/CM8cw_so.js","_app/immutable/chunks/BF7VELds.js","_app/immutable/chunks/BtwZf26V.js","_app/immutable/chunks/D2Ok8g_2.js","_app/immutable/chunks/DyHxZ_1q.js","_app/immutable/chunks/Dg1qgpEv.js","_app/immutable/chunks/DgtScFKl.js","_app/immutable/chunks/CYgJF_JY.js"];
-export const stylesheets = [];
-export const fonts = [];
