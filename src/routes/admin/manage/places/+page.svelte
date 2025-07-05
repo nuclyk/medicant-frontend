93af38b5b0663d6b9a5b7fc7c996d808e009043e
Nucly <nucly@proton.me>
2025-07-05 15:03:34 +0700

    room field for places

--- src/routes/admin/manage/places/+page.svelte
+++ src/routes/admin/manage/places/+page.svelte
@@ -10,11 +10,12 @@
     let apiUrl = getContext("apiUrl");
 
     let placeName = $state("");
+    let placeRoom = $state("");
     let placeCapacity = $state();
 
-    async function handleDelete(name) {
+    async function handleDelete(id) {
         try {
-            const res = await fetch(apiUrl() + "places/" + name, {
+            const res = await fetch(apiUrl() + "places/" + id, {
                 method: "DELETE",
                 headers: {
                     "Content-Type": "application/json",
@@ -26,7 +27,7 @@
                 error(res.status, "Could not delete the place!");
             }
 
-            let index = places().findIndex((place) => place.name === name);
+            let index = places().findIndex((place) => place.id === id);
 
             places().splice(index, 1);
 
@@ -36,9 +37,9 @@
         }
     }
 
-    async function handleChange(name, event) {
+    async function handleChange(id, event) {
         try {
-            const res = await fetch(apiUrl() + "places/" + name, {
+            const res = await fetch(apiUrl() + "places/" + id, {
                 method: "PUT",
                 headers: {
                     "Content-Type": "application/json",
@@ -53,7 +54,7 @@
                 error(res.status, "Could not update place");
             }
 
-            let index = places().findIndex((p) => p.name === name);
+            let index = places().findIndex((p) => p.id === id);
             places()[index] = await res.json();
 
             toast.success("Place updated successfuly!");
@@ -79,24 +80,32 @@
     >
         <p>Add new place</p>
         <div class="row bg-light border rounded-1 p-2 g-2">
-            <div class="col-xxl flex-fill">
+            <div class="col-auto">
                 <input
                     class="form-control"
                     type="text"
                     name="name"
-                    placeholder="Add new place"
+                    placeholder="Place name"
                     bind:value={placeName}
                     required
                 />
             </div>
-            <div class="col-xxl flex-fill">
+            <div class="col">
+                <input
+                    class="form-control"
+                    type="text"
+                    name="room"
+                    placeholder="Room"
+                    bind:value={placeRoom}
+                />
+            </div>
+            <div class="col">
                 <input
                     class="form-control"
                     type="number"
                     name="capacity"
                     placeholder="Capacity"
                     bind:value={placeCapacity}
-                    required
                 />
             </div>
             <div class="col-xxl">
@@ -110,25 +119,38 @@
     <hr class="border border-1 opacity-50" />
 
     <p>Edit places</p>
-    {#each places().filter((p) => p.name != "None") as place (place.name)}
+    {#each places().filter((p) => p.name != "None") as place (place.id)}
         <div class="row g-2 bg-light border rounded-1 p-2 my-2">
-            <div class="col-lg-auto flex-fill">
+            <div class="col-auto input-group">
+                <span class="input-group-text">Name </span>
                 <input
                     class="form-control"
                     type="text"
                     name="name"
                     value={place.name}
-                    onchange={(e) => handleChange(place.name, e)}
+                    onchange={(e) => handleChange(place.id, e)}
                     required
                 />
             </div>
-            <div class="col-lg-auto flex-fill">
+            <div class="col input-group">
+                <span class="input-group-text">Room </span>
+                <input
+                    class="form-control"
+                    type="number"
+                    name="room"
+                    value={place.room}
+                    onchange={(e) => handleChange(place.id, e)}
+                    required
+                />
+            </div>
+            <div class="col input-group">
+                <span class="input-group-text">Capacity </span>
                 <input
                     class="form-control"
                     type="number"
                     name="capacity"
                     value={place.capacity}
-                    onchange={(e) => handleChange(place.name, e)}
+                    onchange={(e) => handleChange(place.id, e)}
                     required
                 />
             </div>
@@ -136,7 +158,7 @@
                 <button
                     type="button"
                     class="btn btn-dark flex-fill w-100"
-                    onclick={() => handleDelete(place.name)}>Delete</button
+                    onclick={() => handleDelete(place.id)}>Delete</button
                 >
             </div>
         </div>
